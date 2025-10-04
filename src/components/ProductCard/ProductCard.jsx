c3822238779b641ca3a5bfdfa880991000411420
Matias1345 <matiaslopez1345@gmail.com>
2025-10-03 21:06:04 -0300

    Carta con imgs y correcion data

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -4,12 +4,16 @@ import styles from "./ProductCard.module.css";
 //producto y onAdd son props que vienen de Carta
 export default function ProductCard({ producto, onAdd }) {
   return (
-    <div className="card shadow-sm h-100" style={{ width: "18rem" }}>
+    <div id="tarjeta" className="card shadow-sm h-100">
       <img
         src={producto.img}
         alt={producto.nombre}
         className="card-img-top"
         style={{ objectFit: "cover", height: "200px" }}
+        //objectFit: "cover" hace que la imagen rellene el espacio 
+        //disponible sin perder proporcion
+        //height es para que todas las imagenes tengan una altura fija de 200px
+        //sin importar su tamanio original
       />
       <div className="card-body d-flex flex-column">
         <h5 className="card-title">{producto.nombre}</h5>
