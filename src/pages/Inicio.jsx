66283dcde574ea79cc4a4d5d910a0e351a956ea9
Pablo Cortese <cortesepablonahuel@gmail.com>
2025-10-05 10:46:34 -0300

    avance con la pagina de inicio

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -3,7 +3,7 @@ import style from "../stylesPages/inicio.module.css"
 
 export default function Inicio() {
   return (
-    <div >
+    <div>
       <div className={style.containerInicio}>
 
         <h2 className={style.subTitulo}>Bienvenido a Luna & Granos Café</h2>
@@ -14,17 +14,25 @@ export default function Inicio() {
           productos, creados para acompañar tus mejores instantes.
         </p>
 
-        <Link to="/carta">
-          Ver Carta
-        </Link>
+        <Link to="/carta">Ver Carta</Link>
       </div>
-      <section>
-        <p>section 1</p>
+      <section className={style.sectionContainer}>
+        <div>
+          <h3>Acerca de nosotros</h3>
+          <p>
+            En Luna & Granos, creemos que el café es más que una bebida: es una forma de conectar. Nuestro espacio está diseñado para inspirar, crear y disfrutar, ya sea que vengas a trabajar, reunirte o simplemente desconectar. Usamos granos de origen, métodos de preparación artesanales y acompañamientos frescos que reflejan nuestro compromiso con la calidad y la comunidad.
+          </p>
+        </div>
+        <img
+          src="/granos_de_caffe.jpg" 
+          alt="" 
+          className={style.aboutUsImg}
+        />
       </section>
-      <section>
+      <section className={style.sectionContainer2}>
         <p>section 2</p>
       </section>
-      <section>
+      <section className={style.sectionContainer3}>
         <p>section 3</p>
       </section>
     </div>
