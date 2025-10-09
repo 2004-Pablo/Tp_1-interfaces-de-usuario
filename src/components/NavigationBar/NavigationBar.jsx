79c60919a89fdb0c5f0bb1aa05e372454a85e836
Matias1345 <matiaslopez1345@gmail.com>
2025-10-08 21:37:50 -0300

    mas estilos y navBar actualizado

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -2,22 +2,23 @@ import { Link, NavLink } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import styles from "./NavigationBar.module.css";
 
 //Ver si uso bootstrap y otra cosa y adaptar segun qué elija
 // <Navbar.Brand as={Link} to="/">Unahur</Navbar.Brand>
 function NavigationBar() {
   return (  
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="/">Inicio</Nav.Link>
-            <Nav.Link as={NavLink} to="/carta">Carta</Nav.Link>
-            <Nav.Link as={NavLink} to="/carrito">Carrito</Nav.Link>
-            <Nav.Link as={NavLink} to="/formulario">Formulario</Nav.Link>
-            <Nav.Link as={NavLink} to="/nosotros">Nosotros</Nav.Link>
+    /*Componentes de react-bootstrap*/
+    <Navbar expand="lg" /*className="bg-body-tertiary"*/>
+      <Container>      
+        <Navbar.Toggle /*aria-controls="basic-navbar-nav"*/ />
+        <Navbar.Collapse /*id="basic-navbar-nav"*/>
+          <Nav /*className="me-auto"*/>
+            <Nav.Link as={NavLink} to="/" className={styles.navLink}>Inicio</Nav.Link>
+            <Nav.Link as={NavLink} to="/carta" className={styles.navLink}>Carta</Nav.Link>
+            <Nav.Link as={NavLink} to="/carrito" className={styles.navLink}>Carrito</Nav.Link>
+            <Nav.Link as={NavLink} to="/formulario" className={styles.navLink}>Formulario</Nav.Link>
+            <Nav.Link as={NavLink} to="/nosotros" className={styles.navLink}>Nosotros</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
